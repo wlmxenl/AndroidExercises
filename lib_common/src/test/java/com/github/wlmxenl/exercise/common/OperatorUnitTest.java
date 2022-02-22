152f4352b5e15be928b03b2bd14cc3fb7d3bd3ba
wangzhengfu <1797301908@139.com>
2022-02-22 18:55:39 +0800

    操作符示例

--- lib_common/src/test/java/com/github/wlmxenl/exercise/common/OperatorUnitTest.java
+++ lib_common/src/test/java/com/github/wlmxenl/exercise/common/OperatorUnitTest.java
@@ -10,6 +10,9 @@ import org.junit.Test;
  */
 public class OperatorUnitTest {
 
+    /**
+     * 位逻辑运算
+     */
     @Test
     public void bitWiseOperator() {
         /*
@@ -57,5 +60,60 @@ public class OperatorUnitTest {
          * 11 -> 对应 10 进制 = 3
          */
         System.out.printf("1 ^ 2 = %s%n", 1 ^ 2);
+    }
+
+
+    /**
+     * 位移运算
+     */
+    @Test
+    public void shiftOperator() {
+        /*
+         * 右移运算 297 >> 5 = 9504
+         *
+         * 297 二进制 = 100101001
+         * 左移5位 = 10010100100000
+         * 1001 转换 10 进制 = 9
+         *
+         * 简化：297 / 2^5 = 9
+         */
+        System.out.printf("297 >> 5 = %s%n", 297 >> 5);
+
+        /*
+         * 左移运算 2 << 5 = 9
+         *
+         * 2 二进制 = 10
+         * 左移5位 = 1000000
+         * 1000000 转换 10 进制 = 64
+         *
+         * 简化：2 * 2^5 = 64
+         */
+        System.out.printf("2 << 5 = %s%n", 2 << 5);
+
+
+        /*
+         * 无符号右移 2 >>> 1 = 1
+         * 等同于 2 >> 1
+         */
+        System.out.printf("2 >>> 1 = %s%n", 2 >>> 1);
+
+
+        /*
+         * 无符号右移最终得到的是正数
+         *
+         * -2 >>> 1 = 2147483647
+         *
+         *
+         */
+        System.out.printf("-2 >>> 1 = %s%n", -2 >>> 1);
+    }
+
+    @Test
+    public void castTest() {
+        // (byte)128 = -128
+        System.out.println((byte)128);
+        // 128 二进制 0000 0000 0000 0000 0000 0000 1000 0000
+        // 强转为 byte 时，就是一个强制高位截断的过程，得到：0000 0000 1000 0000
+        // byte 范围区间值 -128 ~ 127 = 1000 0000 ~ 0111 1111
     }
 }
