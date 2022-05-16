767058612b542b291b3edece6496f496c7626d0a
wangzf <1797301908@139.com>
2022-05-16 15:56:20 +0800

    update

--- exercise-other/src/main/java/cn/dripcloud/exercise/other/MainActivity.kt
+++ exercise-other/src/main/java/cn/dripcloud/exercise/other/MainActivity.kt
@@ -1,7 +1,9 @@
 package cn.dripcloud.exercise.other
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import cn.dripcloud.exercise.other.lottie.LottieSampleActivity
 import com.blankj.utilcode.util.ScreenUtils
 import com.blankj.utilcode.util.SizeUtils
 import com.github.wlmxenl.exercise.common.widget.MaxHeightScrollView
@@ -11,5 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, LottieSampleActivity::class.java))
     }
 }
---
+++ exercise-other/src/main/java/cn/dripcloud/exercise/other/lottie/LottieSampleActivity.kt
@@ -0,0 +1,17 @@
+package cn.dripcloud.exercise.other.lottie
+
+import android.os.Bundle
+import cn.dripcloud.exercise.other.databinding.LottieSampleActivityBinding
+import com.github.wlmxenl.exercise.common.page.BaseActivity
+
+/**
+ *
+ * @author wangzf
+ * @date 2022/5/16
+ */
+class LottieSampleActivity : BaseActivity<LottieSampleActivityBinding>() {
+
+    override fun onPageViewCreated(savedInstanceState: Bundle?) {
+
+    }
+}
